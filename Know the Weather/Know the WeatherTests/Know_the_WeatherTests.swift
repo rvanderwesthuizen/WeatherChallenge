b47f119d75bda16f3b51fcd241ff09a509ea14e5
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-08-27 12:09:39 +0200

    First Commit: Creating the project

---
+++ Know the Weather/Know the WeatherTests/Know_the_WeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  Know_the_WeatherTests.swift
+//  Know the WeatherTests
+//
+//  Created by Ruan van der Westhuizen on 2021/08/27.
+//
+
+import XCTest
+@testable import Know_the_Weather
+
+class Know_the_WeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
