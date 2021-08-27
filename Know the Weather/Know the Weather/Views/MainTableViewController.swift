f09f36998da7639fd61e9a6e9ad1048d6995d604
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-08-27 12:35:13 +0200

    Created folders to comply to MVVM and changed to a tableview

---
+++ Know the Weather/Know the Weather/Views/MainTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTableViewController.swift
+//  Know the Weather
+//
+//  Created by Ruan van der Westhuizen on 2021/08/27.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    //MARK: - Table view delegate
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
